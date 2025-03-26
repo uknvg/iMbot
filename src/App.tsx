641e15037bd696210fe14d011220e58af9f110b9
uknvg <kamj13.mj@gmail.com>
2025-03-26 21:38:08 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
             } 
           />
           <Route 
-            path="/dashboard" 
+            path="iMbot/dashboard" 
             element={
               <>
                 <Home />
